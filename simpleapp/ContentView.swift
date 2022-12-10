846c50ada64b57ef3c00c14e8d507c54c5ca6221
A LEELA KRISHNA REDDY <113173306+krishboy@users.noreply.github.com>
2022-12-10 23:16:30 +0530

    Initial Commit

---
+++ simpleapp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  simpleapp
+//
+//  Created by A.Leelakrishna Reddy on 10/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ simpleapp/simpleappApp.swift
@@ -0,0 +1,17 @@
+//
+//  simpleappApp.swift
+//  simpleapp
+//
+//  Created by A.Leelakrishna Reddy on 10/12/22.
+//
+
+import SwiftUI
+
+@main
+struct simpleappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
